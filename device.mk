299dea17a8bf889a52cd9f2347a40d79248b2ca7
bang63 <cakeguay@hotmail.com>
2017-08-14 19:39:27 +0200

    Update and rename device.mk.template to device.mk

--- device.mk
+++ device.mk
@@ -3,13 +3,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
-$(call inherit-product-if-exists, vendor/__MANUFACTURER__/__DEVICE__/__DEVICE__-vendor.mk)
+$(call inherit-product-if-exists, vendor/bq/freezerfhd/freezerfhd-vendor.mk)
 
-DEVICE_PACKAGE_OVERLAYS += device/__MANUFACTURER__/__DEVICE__/overlay
+DEVICE_PACKAGE_OVERLAYS += device/bq/freezerfhd/overlay
 
 
 ifeq ($(TARGET_PREBUILT_KERNEL),)
-	LOCAL_KERNEL := device/__MANUFACTURER__/__DEVICE__/kernel
+	LOCAL_KERNEL := device/bq/freezerfhd/kernel
 else
 	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
 endif
@@ -20,5 +20,5 @@ PRODUCT_COPY_FILES += \
 $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
-PRODUCT_NAME := full___DEVICE__
-PRODUCT_DEVICE := __DEVICE__
+PRODUCT_NAME := full_freezerfhd
+PRODUCT_DEVICE := freezerfhd
